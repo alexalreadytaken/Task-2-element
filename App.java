203e86d65ea40bb56717c29d19da73756abc3fc7
Alex <nope>
2021-05-06 11:28:10 +0500

    0.4 crutch of crutch finish

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.IntStream;
 
 public class App {
@@ -19,24 +18,24 @@ public class App {
     private static final ElementExampleImpl.Context context_100_000 = new ElementExampleImpl.Context();
 
     static {
-        for (int i = 0; i < 100; i++)elements_100.add(new ElementExampleImpl(context_100,i));
+        for (int i = -10; i < 100; i++)elements_100.add(new ElementExampleImpl(context_100,i));
         for (int i = 0; i < 1000; i++)elements_1000.add(new ElementExampleImpl(context_1000,i));
         for (int i = 0; i < 10_000; i++)elements_10_000.add(new ElementExampleImpl(context_10_000,i));
         for (int i = 0; i < 100_000; i++)elements_100_000.add(new ElementExampleImpl(context_100_000,i));
     }
 
     public static void main(String[] args) {
-        String result1 = testWithShuffle(Task2Impl.INSTANCE_1, 1, elements_100, context_100);
+        String result1 = testWithShuffle(Task2Impl.getInstance(elements_100), 1, elements_100, context_100);
         System.out.println(result1);
 
-        /*String result2 = testWithShuffle(Task2Impl.INSTANCE_1, 1, elements_1000, context_1000);
+        String result2 = testWithShuffle(Task2Impl.getInstance(elements_1000), 1, elements_1000, context_1000);
         System.out.println(result2);
 
-        String result3 = testWithShuffle(Task2Impl.INSTANCE_1, 1, elements_10_000, context_10_000);
+        String result3 = testWithShuffle(Task2Impl.getInstance(elements_10_000), 1, elements_10_000, context_10_000);
         System.out.println(result3);
 
-        String result4 = testWithShuffle(Task2Impl.INSTANCE_1, 1, elements_100_000, context_100_000);
-        System.out.println(result4);*/
+        String result4 = testWithShuffle(Task2Impl.getInstance(elements_100_000), 1, elements_100_000, context_100_000);
+        System.out.println(result4);
     }
 
     @SuppressWarnings("all")
@@ -45,11 +44,12 @@ public class App {
         for (int i = 0; i < testCount; i++) {
             Collections.shuffle(elements);
             assigner.assignNumbers(elements);
+            for (int j = 0; j < elements.size(); j++) {
+                if (elements.get(j).getNumber()!=j)
+                    throw new IllegalStateException("assginer invalid");
+            }
         }
         long end = System.currentTimeMillis();
-        boolean assignerWork = IntStream.range(0, elements.size())
-                .allMatch(i -> elements.get(i).getNumber() == i);
-        if (!assignerWork)throw new IllegalStateException("assigner invalid :)");
         int operationCount = context.getOperationCount();
         double avgOperationsCount = (double) operationCount / testCount;
         double performance = (double) end - start;
--- Task2Impl.java
+++ Task2Impl.java
@@ -1,6 +1,8 @@
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toSet;
 
 
 /**
@@ -14,80 +16,118 @@ import java.util.function.Consumer;
  */
 public enum Task2Impl implements IElementNumberAssigner {
 
-    INSTANCE_1;
+    /**
+     * алгоритм находит замкнутые цепи если они есть и работает внутри них
+     * <blockquote>i-0 === n-2; 1.1 - цепь началась</blockquote>
+     * <blockquote>i-1 === n-3; </blockquote>
+     * <blockquote>i-2 === n-(-1);1.3 - кандидат найден, так как -1 точно не занята ставим ему 2,
+     * -1 временно ставим (i-5) элементу</blockquote>
+     * <blockquote>i-3 === n-4; </blockquote>
+     * <blockquote>i-4 === n-1; </blockquote>
+     * <blockquote>i-5 === n-0; 1.2 - элементу ставится {@link Task2Impl#buffer},
+     * нулевому элементу ставится 0, 2 остается свободной,ищем кандидата на присвоение двойки</blockquote>
+     * <blockquote>i-6 === n-5; </blockquote>
+     */
+    //так и не смог пофиксить невозможность работы с отрицательными числами
+    POSITIVE_NUMBERS_FIXER{
+        @Override
+        protected void fix(List<IElement> elements, IElement element, int index) {
+            //нужен что-бы не было stackoverflow при цепи больше 1024 элементов
+            Stack<Consumer<Void>> stack = new Stack<>();
+            if (element!=null){
+                for (int i = 0; i < elements.size(); i++) {
+                    IElement innerElem = elements.get(i);
+                    if (innerElem.getNumber() == index) {
+                        innerElem.setupNumber(super.buffer);
+                        int unusedNumber = element.getNumber();
+                        element.setupNumber(index);
+                        int finalI = i;
+                        stack.push((unusedArgument)->fixBuffer(elements,innerElem,finalI,unusedNumber,stack));
+                        while (!stack.empty()){
+                            stack.pop().accept(null);
+                        }
+                    }
+                }
+                element.setupNumber(index);
+            }
+        }
 
-    private final Integer buffer = Integer.MIN_VALUE;
+        private void fixBuffer(List<IElement> elements, IElement element, int index, int unusedNumber,Stack<Consumer<Void>> stack) {
+            for (int i = 0; i < elements.size(); i++) {
+                if (i==unusedNumber){
+                    IElement innerElem = elements.get(i);
+                    if (innerElem.getNumber()==index){
+                        innerElem.setupNumber(unusedNumber);
+                        element.setupNumber(index);
+                    }else {
+                        int unusedNumber0 = innerElem.getNumber();
+                        innerElem.setupNumber(unusedNumber);
+                        stack.push((unusedArgument)->fixBuffer(elements,element,index,unusedNumber0,stack));
+                    }
+                    return;
+                }
+            }
+            element.setupNumber(unusedNumber);
+        }
+    },
+    /**
+     * просто проходится по списку меняя пару где есть неправильное число
+     */
+    ALL_NUMBERS_FIXER{
+        @Override
+        protected void fix(List<IElement> elements, IElement element, int index) {
+            elements.stream()
+                    .parallel()
+                    .filter(elem -> elem.getNumber() == index && elem != element)
+                    .findFirst()
+                    .ifPresentOrElse(elem -> {
+                        int number = element.getNumber();
+                        elem.setupNumber(super.buffer);
+                        element.setupNumber(index);
+                        elem.setupNumber(number);
+                    }, () -> element.setupNumber(index));
+        }
+    };
 
+    private Integer buffer;
 
-    @Override
-    public void assignNumbers(final List<IElement> elements) {
-        if (elements == null) return;
+    public static IElementNumberAssigner getInstance(List<IElement> elements){
         boolean containsNegativeNumbers = elements.stream()
                 .anyMatch(el -> 0 > el.getNumber());
         if (containsNegativeNumbers){
             System.err.println("list contains negative numbers,performance will slow down :(");
+            return ALL_NUMBERS_FIXER;
+        }else {
+            return POSITIVE_NUMBERS_FIXER;
         }
+    }
+
+    @Override
+    public void assignNumbers(final List<IElement> elements) {
+        if (elements == null) return;
+        makeBuffer(elements);
         for (int i = 0; i < elements.size(); i++) {
             IElement element = elements.get(i);
             if (element.getNumber() != i) {
-                if (containsNegativeNumbers) {
-                    plainFix(elements,element,i);
-                }else {
-                    fixChain(elements,element,i);
-                }
+                fix(elements,element,i);
             }
         }
     }
 
-    private void fixChain(List<IElement> elements, IElement element, int index){
-        Stack<Consumer<Void>> stack = new Stack<>();
-        if (element!=null){
-            for (int i = 0; i < elements.size(); i++) {
-                IElement innerElem = elements.get(i);
-                if (innerElem.getNumber() == index) {
-                    innerElem.setupNumber(buffer);
-                    int unusedNumber = element.getNumber();
-                    element.setupNumber(index);
-                    int finalI = i;
-                    stack.push((unusedArgument)->fixBuffer(elements,innerElem,finalI,unusedNumber,stack));
-                    while (!stack.empty()){
-                        stack.pop().accept(null);
-                    }
-                }
-            }
-            element.setupNumber(index);
-        }
+    protected void fix(List<IElement> elements, IElement element, int index){
+        System.err.println("no default realization");
     }
 
-    private void fixBuffer(List<IElement> elements, IElement element, int index, int unusedNumber,Stack<Consumer<Void>> stack) {
-        for (int i = 0; i < elements.size(); i++) {
-            if (i==unusedNumber){
-                IElement innerElem = elements.get(i);
-                if (innerElem.getNumber()==index){
-                    innerElem.setupNumber(unusedNumber);
-                    element.setupNumber(index);
-                }else {
-                    int unusedNumber0 = innerElem.getNumber();
-                    innerElem.setupNumber(unusedNumber);
-                    int finalI = i;
-                    stack.push((unusedArgument)->fixBuffer(elements,innerElem,finalI,unusedNumber0,stack));
-                }
-                return;
-            }
-        }
-    }
-
-    private void plainFix(List<IElement> elements, IElement element, int index) {
-        elements.stream()
+    protected void makeBuffer(List<IElement> elements){
+        Set<Integer> numbers = elements.stream()
                 .parallel()
-                .filter(elem -> elem.getNumber() == index && elem != element)
-                .findFirst()
-                .ifPresentOrElse(elem -> {
-                    int number = element.getNumber();
-                    elem.setupNumber(buffer);
-                    element.setupNumber(index);
-                    elem.setupNumber(number);
-                }, () -> element.setupNumber(index));
+                .map(IElement::getNumber)
+                .collect(toSet());
+        int possibleBuffer = Integer.MIN_VALUE;
+        do {
+            ++possibleBuffer;
+        }while (numbers.contains(possibleBuffer));
+        buffer = possibleBuffer;
     }
 
 }
